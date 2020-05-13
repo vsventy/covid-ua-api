2c3f8d00e94869d4cf6c2f30d725ccd071817552
Vitalii Sventyi <vsventy@gmail.com>
2020-05-13 19:27:11 +0300

    update scheduler config

--- covidapi/celery_app.py
+++ covidapi/celery_app.py
@@ -1,16 +1,14 @@
 from celery.schedules import crontab
 
 from covidapi.app import init_celery
-from covidapi.tasks.covid_parser import run_covid_parser
 
 app = init_celery()
 app.conf.imports = app.conf.imports + ("covidapi.tasks.covid_parser",)
 
-
-@app.on_after_configure.connect()
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_tasks(
-        crontab(hour=7, minute=0),
-        run_covid_parser,
-        name='run covid parser at 07:00 (UTC) everyday'
-    )
+app.conf.beat_schedule = {
+    'run-parser-everyday-at-7-00': {
+        'task': 'covidapi.tasks.covid_parser.run_covid_parser',
+        'schedule': crontab(hour=7, minute=0),
+    },
+}
+app.conf.timezone = 'UTC'
